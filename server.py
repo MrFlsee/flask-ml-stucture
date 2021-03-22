4394cf18c07ad2136cbd8c8ab2e7c8211a66e1e4
Sangwoo Kang <swoo@Sangwooui-iMac.Davolink>
2021-03-22 14:30:13 +0900

    + first commit

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask, request
+from flask_restx import Resource, Api
+from todo import Todo
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_namespace(Todo, '/todos')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=3154)
+
+
